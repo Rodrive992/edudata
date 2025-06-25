da7f62106e06139d1727718f7d77d020d1963cdb
Rodri <rodrigovegga992@gmail.com>
2025-06-25 15:38:37 -0300

    Corrijo base en Vite para evitar contenido mixto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: 'https://web-production-10480.up.railway.app/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
