f1a17cae4aec8de2eb46bb65f6932958c2a7f1de
Rodri <rodrigovegga992@gmail.com>
2025-10-22 09:18:09 -0300

    Estilos3

--- resources/views/edured/partials/admin-content.blade.php
+++ resources/views/edured/partials/admin-content.blade.php
@@ -1,14 +1,24 @@
 <div class="space-y-8">
     <div>
-        <h1 class="text-2xl font-bold mb-2">Panel de Administrador</h1>
-        <p class="text-gray-600">Seleccione una herramienta para administrar módulos y cargas.</p>
+        <h1 class="text-2xl font-bold mb-2">Panel de control y carga -  Dirección de Transparencia Activa</h1>
+        <p class="text-gray-600">Seleccione una herramienta para administrar secciones de Edudata.</p>
     </div>
 
     <!-- Herramientas -->
     <div class="bg-white rounded-xl shadow border border-gray-200 p-6">
         <h2 class="text-lg font-semibold text-gray-800 mb-4">Herramientas</h2>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+             <!-- Botón real: Solicitudes info-->
+            <a href="{{ route('edured.herramientas.solicitudes-info.index') }}"
+               class="group flex items-center justify-between p-4 rounded-xl border hover:shadow-md transition
+                      border-gray-200 hover:border-gray-300 bg-gray-50 hover:bg-white">
+                <div>
+                    <p class="font-semibold text-gray-800">Solicitudes de Información</p>
+                    <p class="text-sm text-gray-500">Gestión de las solicitudes</p>
+                </div>
+                <span class="text-gray-400 group-hover:text-gray-600">➜</span>
+            </a>
             <!-- Botón real: Cargar Digesto -->
             <a href="{{ route('edured.herramientas.digesto.index') }}"
                class="group flex items-center justify-between p-4 rounded-xl border hover:shadow-md transition
@@ -25,13 +35,13 @@
                class="group flex items-center justify-between p-4 rounded-xl border hover:shadow-md transition
                       border-gray-200 hover:border-gray-300 bg-gray-50 hover:bg-white">
                 <div>
-                    <p class="font-semibold text-gray-800">Solicitudes Edilicia</p>
-                    <p class="text-sm text-gray-500">Gestión de solicitudes</p>
+                    <p class="font-semibold text-gray-800">Gestión de Usuarios</p>
+                    <p class="text-sm text-gray-500">Crear, editar y eliminar usuarios Edured</p>
                 </div>
                 <span class="text-gray-400 group-hover:text-gray-600">➜</span>
             </a>
 
-            <a href="#"
+            <a href="{{ route('edured.herramientas.mantenimiento.realizadas.create') }}"
                class="group flex items-center justify-between p-4 rounded-xl border hover:shadow-md transition
                       border-gray-200 hover:border-gray-300 bg-gray-50 hover:bg-white">
                 <div>
